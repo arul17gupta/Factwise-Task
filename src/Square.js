32fc5a858fece2914840e993736fcf64d250fa0a
Arul Gupta <arul17gupta@gmail.com>
2024-07-09 12:57:53 +0530

    Task Completed

---
+++ src/Square.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Square({value, onClick}){
+    return (
+        <div>
+            <button className="square" onClick={onClick}>
+                {value}
+            </button>
+        </div>
+    );
+}
+
+
+export default Square;
